72f4e8166dd164104dcfe16b5a7357b3c9de39dc
Alexisp1217i <aldabaalexis85@gmail.com>
2024-09-05 16:18:20 -0600

    Update EVA1_9_CONVERCIONES.java

--- EVA1_9_CONVERCIONES/src/main/java/com/mycompany/eva1_9_converciones/EVA1_9_CONVERCIONES.java
+++ EVA1_9_CONVERCIONES/src/main/java/com/mycompany/eva1_9_converciones/EVA1_9_CONVERCIONES.java
@@ -14,18 +14,25 @@ public class EVA1_9_CONVERCIONES {
 
     public static void main(String[] args) {
 
-        double farenheit, centigrados;
+        double fahrenheit, centigrados, kelvin;
         Scanner captu = new Scanner(System.in);
-        System.out.println("Temperatura en grados farenheit:");
-        farenheit = captu.nextDouble();
-        centigrados = (farenheit - 32) / 1.8;
+        System.out.println("Temperatura en grados fahrenheit:");
+        fahrenheit = captu.nextDouble();
+        centigrados = (fahrenheit - 32) / 1.8;
         System.out.println("° =" + centigrados);
         
-        //De centigrados a farenheit
+        //De centigrados a fahrenheit
          System.out.println("Temperatura en grados centigrados:");
         centigrados = captu.nextDouble();
-        farenheit = centigrados * 1.8 + 1.8;
-        System.out.println("°F =" + farenheit);
+        fahrenheit = centigrados * 1.8 + 1.8;
+        System.out.println("°F =" + fahrenheit);
+        
+        //De fahrenheit a kelvin
+        System.out.println("Temperatura en grados kelvin:");
+        fahrenheit = captu.nextDouble();
+        kelvin = ((fahrenheit - 32) / 1.8) + 273.15;
+        System.out.println("°K =" + kelvin);
+        
         
         
         
